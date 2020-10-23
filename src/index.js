c1b5e69f2733c12a214a5b1fd5e90e8506702fc4
MarilenaRoque <marilenas@id.uff.br>
2020-10-23 19:53:52 -0300

    Setting first structure for Menu Section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './style/style.css'
 import load from "./load"
+import homeSection from "./home"
+import menuSection from "./menu"
 
 load();
+menuSection();
--- src/menu.js
+++ src/menu.js
@@ -1,29 +1,33 @@
 import family from "./images/bakery.jpg"
 const homeText = "Roque's Bakery is defined by a focus on the craft of traditional Italian baking methods. Inspired by iconic shops such as Roscioli in Rome—a family-owned bakery, café, grocer, and restaurant dating back to 1824—the team works the “old way,” using techniques often sacrificed in favor of time or ease of production. Flour for the focaccia dough is milled in-house; cannoli are filled to order; quality is never compromised. "
 
-const homeSection = () => {
+const menuSection = () => {
     const divSections = document.getElementById("sections");
     divSections.innerHTML = "";
 
-    //Set H1
     const homeHeading = document.createElement('h1');
-    homeHeading.innerText = "Roque's Bakery";
+    homeHeading.innerText = "Menu";
     divSections.append(homeHeading);
 
-    // Set P
-    const homeParagraph = document.createElement('p');
-    homeParagraph.innerText = homeText;
-    divSections.append(homeParagraph);
+    // Set new Div
+    const divMenu = document.createElement('div');
+    divMenu.id = "div-menu";
 
-    //Family Image
-    const familyPicture = new Image();
-    familyPicture.src = family;
-    familyPicture.id = "bakery"
-    familyPicture.setAttribute('width', "300px");
-    divSections.append(familyPicture);
+    const breakfast = document.createElement('ul');
+    const ulContent = "<li>YOGURT BERRY PARFAIT</li><li>OVERNIGHT OATS & CHIA</li><li>MIXED BERRIES</li>";
+    breakfast.innerHTML = ulContent;
+    divMenu.prepend(breakfast);
 
+    divSections.append(divMenu);
+
+
+    // const ulContent = "<li></li>";
+    // const myUl = document.createElement('ul');
+    // myUl.innerHTML = ulContent;
+    // myUl.id = "buttons";
+    // divContent.prepend(myUl);
 
 }
 
 
-export default homeSection ;
+export default menuSection ;
