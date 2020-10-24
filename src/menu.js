f4f72897ed91be34e703e580e3acabb55e03a13f
MarilenaRoque <marilenas@id.uff.br>
2020-10-24 00:00:00 -0300

    Refactor HomeSection Function
    Refactor code to have a single Manipulation to the Dom

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,8 @@
 import family from "./images/bakery.jpg"
 const homeText = "Roque's Bakery is defined by a focus on the craft of traditional Italian baking methods. Inspired by iconic shops such as Roscioli in Rome—a family-owned bakery, café, grocer, and restaurant dating back to 1824—the team works the “old way,” using techniques often sacrificed in favor of time or ease of production. Flour for the focaccia dough is milled in-house; cannoli are filled to order; quality is never compromised. "
-
 const menuSection = () => {
     const divSections = document.getElementById("sections");
     divSections.innerHTML = "";
-
-    const homeHeading = document.createElement('h1');
-    homeHeading.innerText = "Menu";
-    divSections.append(homeHeading);
 
     // Set new Div
     const divMenu = document.createElement('div');
